0275a1d9af577f639e3cf7d637403ea5fd81f4b9
Saisri Gottimukkula <saisrigottimukkula@gmail.com>
2025-06-30 10:58:09 +0530

    commit

--- src/ex_11_java_Test/Lab005_Java_Test_05.java
+++ src/ex_11_java_Test/Lab005_Java_Test_05.java
@@ -1,15 +1,22 @@
 package ex_11_java_Test;
 
+import java.util.Scanner;
+
 public class Lab005_Java_Test_05 {
     public static void main(String[] args){
-        int a = 10;
-        int b = 5;
-        System.out.println("a==b :" + (a==b));
-        System.out.println("a>b :" + (a>b));
-        System.out.println("a<b :" + (a<b));
-        System.out.println("(a > b) && (a > 0) :" + ((a > b) && (a > 0)));
-        System.out.println("(a < b) || (a > 0) :" + ((a < b) || (a > 0)));
-        System.out.println("!(a>b) :" + (!(a>b)));
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer and a decimal number");
+        int integer = sc.nextInt();
+        double decimal = sc.nextDouble();
+
+        int expression_result = integer + (int)(integer+decimal);
+        System.out.println("Expression result: " + expression_result);
+
+        double implicit_casting = integer + decimal;
+        System.out.println("Implicit casting: " + implicit_casting);
+
+        int explicit_casting = integer + (int)decimal;
+        System.out.println("explicit_casting: " + explicit_casting);
 
     }
 }
--- src/ex_11_java_Test/Lab007_Java_Test_07.java
+++ src/ex_11_java_Test/Lab007_Java_Test_07.java
@@ -3,23 +3,41 @@ package ex_11_java_Test;
 import java.util.Scanner;
 
 public class Lab007_Java_Test_07 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter three numbers a, b, c");
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
 
-        if(a>b && a>c){
-            System.out.println("a is greater");
+        if ((a == b) && (b == c)) {
+            System.out.println("All are equal");
         }
-        else if (b>a && b>c) {
-            System.out.println("b is greater");
-        }
-        else{
-            System.out.println("c is greater");
+
+        else if (a == b && a > c) {
+            System.out.println("Largest Number is: " + a);
         }
 
 
+        else if(a == c && a > b) {
+            System.out.println("Largest Number is: " + a);
+        }
+
+        else if(b == c && b > a) {
+            System.out.println("Largest Number is: " + b);
+        }
+
+        else if(a>b && a>c){
+            System.out.println("Largest Number is: " + a);
+        }
+
+        else if(b>a && b>c) {
+            System.out.println("Largest Number is: " + b);
+        }
+
+        else if(c>a && c>b){
+            System.out.println("Largest Number is: " + c);
+        }
     }
+
 }
