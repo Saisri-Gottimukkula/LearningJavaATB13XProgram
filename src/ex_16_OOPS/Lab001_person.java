77b9805c6814b3d83d4f80e07984f318fabb46bb
Saisri Gottimukkula <saisrigottimukkula@gmail.com>
2025-06-30 11:29:35 +0530

    commit

---
+++ src/ex_16_OOPS/Lab001_person.java
@@ -0,0 +1,31 @@
+package ex_16_OOPS;
+
+public class Lab001_person {
+
+    //these are called the attributes, properties, instance variables, members variables
+        String name;
+        int height;
+        int age;
+        String gender;
+        int weight;
+        boolean is_married;
+
+
+//these are called the behaviour, methods, functions, member functions
+
+        void walk(){
+            System.out.println("walking");
+        }
+
+        void eat(){
+            System.out.println("sleeping");
+        }
+
+        void sleep(){
+        System.out.println("sleeping");
+        }
+
+}
+
+//this is a class which is working as a template
+//class - blueprint
